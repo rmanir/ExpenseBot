e0c5db9247521d0a6b054fd9b3b543e06760b8a0
rmanir <maniraju9295@gmail.com>
2025-11-30 08:40:15 +0530

    encode

---
+++ encode.py
@@ -0,0 +1,11 @@
+import base64
+import json
+
+# Load JSON file
+with open("service-account.json", "r") as f:
+    data = f.read()
+
+# Encode to Base64
+encoded = base64.b64encode(data.encode("utf-8")).decode("utf-8")
+
+print(encoded)
